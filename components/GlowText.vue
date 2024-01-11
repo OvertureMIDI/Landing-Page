36d36b43c6b059f8828eca3f17115fc8fe8f7a15
6gh <47262177+6gh@users.noreply.github.com>
2024-01-10 23:21:24 -0500

    Add initial project files

---
+++ components/GlowText.vue
@@ -0,0 +1,18 @@
+<template>
+    <span
+        class="inline text-primary-content transition-all underline hover:decoration-transparent hover:drop-shadow-[0_0_10px_#fff] duration-1000">
+        <slot />
+    </span>
+</template>
+
+<style>
+@keyframes glow {
+    from {
+        text-shadow: 0 0 0px #fff, 0 0 0px #fff;
+    }
+
+    to {
+        text-shadow: 0 0 20px #fff, 0 0 30px #fff;
+    }
+}
+</style>
---
+++ components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="hero min-h-screen bg-contain"
+        style="background-image: url('https://media1.tenor.com/m/drzLhx3YyrIAAAAd/never_ending.gif');">
+        <div class="hero-overlay bg-base-100 bg-opacity-[0.99]"></div>
+        <div class="hero-content text-center">
+            <div class="max-w-xl">
+                <h1 class="text-5xl font-bold">Coming Soon</h1>
+                <p class="py-6 text-2xl">
+                    The <GlowText>Encyclopedia Black MIDIca</GlowText>
+                    is currently a work in progress. Stay tuned for updates.
+                </p>
+                <p class="py-6 text-2xl">
+                    For now, visit our guides website.
+                </p>
+                <a href="https://guides.blackmidi.wiki">
+                    <button class="text-xl btn btn-primary">Visit guides</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
